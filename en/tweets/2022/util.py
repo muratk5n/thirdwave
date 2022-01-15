17973a2f9e6a93f2babb13f4bfc7057795a8ec69
MK <mm@post.com>
2022-01-15 22:51:53 +0300

    -

---
+++ en/tweets/2022/util.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+def biden_approval():
+    url = "https://projects.fivethirtyeight.com/biden-approval-data/approval_topline.csv"
+    df = pd.read_csv(url,parse_dates=True,index_col='modeldate')
+    df = df[df.subgroup=='All polls']
+    df = df.sort_index()
+    df = df[df.index > '2021-06-01']
+    df['net'] = df['approve_estimate'] - df['disapprove_estimate']
+    return df
+
+def trump_approval():
+    url = "https://projects.fivethirtyeight.com/trump-approval-data/approval_polllist.csv"
+    df = pd.read_csv(url,parse_dates=True,index_col='modeldate')
+    df = df[df.subgroup=='All polls']
+    df = df[df.pollster=='Gallup']
+    df = df.sort_index()
+    df['net'] = df['approve'] - df['disapprove']
+    return df
+
+
