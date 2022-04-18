7ba2355e2160df41948c1034a0d6aab1dfae058b
MK <mm@post.com>
2022-04-18 08:23:00 +0300

    --

--- en/tweets/2022/util.py
+++ en/tweets/2022/util.py
@@ -55,9 +55,8 @@ def trump_approval():
     df['net'] = df['approve'] - df['disapprove']
     return df
 
-def ukr_war_map(dt):
+def fetch_ukr_war_map(dt):
     base = "https://www.understandingwar.org/sites/default/files/DraftUkraineCoT"
-    dt = datetime.datetime(2022,4,16)
     sd = dt.strftime("%B%-d,%Y")
     url = base + sd + ".png"
     outfile = "/tmp/isw-ukr-%d%d%d.png" % (dt.year,dt.month,dt.day)
