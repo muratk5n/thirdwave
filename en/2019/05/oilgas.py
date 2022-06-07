df4217b78fda6922e3cacba79bd9955473f48b8b
MK <mm@post.com>
2022-06-07 12:06:52 +0300

    --

--- en/2019/05/oilgas.py
+++ en/2019/05/oilgas.py
@@ -3,17 +3,21 @@ import folium
 
 m = folium.Map(location=[30, 20], zoom_start=3, tiles="Stamen Terrain")
 
+def plot_rows(df):
+    for index, row in df.iterrows():
+        color = 'red'
+        if 'Gas' in row['FIELD_TYPE']:
+           color = 'blue'
+        folium.CircleMarker(
+            [row['LAT_DD'], row['LON_DD']], 
+            color=color,
+            tooltip=row['FLD_NAME'] + " " + str(int(row['EUR_MMBOE'])) + " mmboe (" + row['FIELD_TYPE'] + ")",
+            radius=3
+        ).add_to(m)
+
 df = pd.read_csv('oilgas-2018.csv')
-
-for index, row in df.iterrows():
-    color = 'red'
-    if row['FIELD_TYPE']=='Gas':
-       color = 'blue'
-    folium.CircleMarker(
-        [row['LAT_DD'], row['LON_DD']], 
-        color=color,
-        tooltip=row['FLD_NAME'] + " " + str(int(row['EUR_MMBOE'])) + " mmboe",
-        radius=2
-    ).add_to(m)
-
+plot_rows(df)
+df = pd.read_csv('oilgas-plus.csv')
+plot_rows(df)
+        
 m.save('oilgas-out.html')
