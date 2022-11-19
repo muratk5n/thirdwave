aa47d175fbcbac1da7a389c0119b85fb2263ad73
MK <mm@post.com>
2022-11-19 19:56:54 +0300

    --

--- en/tweets/2022/tst.md
+++ en/tweets/2022/tst.md
@@ -1,35 +1,51 @@
 
+
+https://www.boxofficemojo.com/search/?q=black+panther
+
+https://www.boxofficemojo.com/title/tt1825683/
+
+
+
+```python
+u.boxofficemojo("Black Panther")
+```
+
+```text
+Out[1]: 
+{'Domestic Opening': '$202,003,951',
+ 'Domestic': '$700,426,566',
+ 'International': '$681,822,260',
+ 'Worldwide Total': '$1,382,248,826'}
+```
+
+```python
+u.boxofficemojo("Black Panther Wakanda Forever")
+```
+
+```text
+Out[1]: 
+{'Domestic Opening': '$181,339,761',
+ 'Domestic': '$220,692,647',
+ 'International': '$187,200,000',
+ 'Worldwide Total': '$407,892,647',
+ 'Release Date': 'November 9, 2022'}
+```
+
+
+
+
+
+
+
 ```python
 import util as u
 import pandas as pd
 pd.set_option('display.max_columns', None)
 ```
 
-
-
 ```python
-u.rent_housing()
-plt.savefig('/tmp/out2.png')    
+u.rotten_tomatoes("Black Panther 2018")
 ```
-
-```text
-             incrent   inchouse
-DATE                           
-2022-05-01  5.215898  15.054403
-2022-06-01  5.776910  12.788382
-2022-07-01  6.307304  10.627432
-2022-08-01  6.740728   9.526485
-2022-09-01  7.206730   8.447235
-2022-10-01  7.521848   7.389046
-```
-
-
-
-
-
-
-
-
 
 
 
@@ -39,30 +55,3 @@ u.sm_plot_ukr2('ukrdata/alt1-1115.csv','ukrdata/alt1-1002.csv',geo)
 plt.savefig('/tmp/out.png')
 ```
 
-
-```python
-u.yf_eps("WMT")
-```
-
-```text
-Out[1]: 
-              startdatetime  epsestimate  epsactual
-6  2022-08-16T07:02:00.000Z         1.62       1.77
-7  2022-05-17T07:02:00.000Z         1.48       1.30
-8  2022-02-17T07:11:00.000Z         1.50       1.53
-```
-
-
-```python
-u.yf_profit("TWTR")
-```
-
-```text
-Out[1]: 
-Breakdown  grossProfit totalRevenue grossProfitMargin
-endDate                                              
-2021-09-30     799.338      1283.82           62.2626
-2021-12-31     1052.13      1567.22           67.1335
-2022-03-31     693.534      1200.98           57.7471
-2022-06-30     635.984      1176.66           54.0499
-```
--- en/tweets/2022/util.py
+++ en/tweets/2022/util.py
@@ -2,12 +2,46 @@ import pandas as pd, datetime, numpy as np, requests
 import requests, urllib.parse, json
 from pandas_datareader import data, wb
 import matplotlib.pyplot as plt, math, yf
-
 import simplegeomap as sm, util
-import datetime, time as timelib
+import datetime, time as timelib, re
 import urllib.request as urllib2
 from io import BytesIO
 import pandas_ta as ta
+
+def boxofficemojo(q):
+    q = q.replace(" ","+").lower()
+    url = "https://www.boxofficemojo.com/search/?q=" + q
+    res = urllib2.urlopen(url).read().decode('utf-8')
+    reres = re.findall('a-size-medium a-link-normal a-text-bold" href="(.*?)"',res)
+    url2 = "https://www.boxofficemojo.com" + reres[0]
+    res2 = urllib2.urlopen(url2).read().decode('utf-8')
+
+    regex2 = 'a-section a-spacing-none mojo-performance-summary-table.*?Domestic.*?money">(.*?)<'
+    domestic = re.findall(regex2,res2,re.DOTALL)[0]
+    regex2 = 'a-section a-spacing-none mojo-performance-summary-table.*?International.*?money">(.*?)<'
+    intl = re.findall(regex2,res2,re.DOTALL)[0]
+    regex2 = 'a-section a-spacing-none mojo-performance-summary-table.*?Worldwide.*?money">(.*?)<'
+    worldwide = re.findall(regex2,res2,re.DOTALL)[0]
+    regex2 = 'Domestic Opening.*?money">(.*?)<'
+    domopen = re.findall(regex2,res2,re.DOTALL)[0]
+    regex2 = '<span>Earliest Release Date</span><span>(.*?)\n.*?</span>'
+    reldate = re.findall(regex2,res2,re.DOTALL)[0]
+    return {"Domestic Opening": domopen, "Domestic": domestic,
+            "International": intl, "Worldwide Total": worldwide,
+            "Release Date": reldate}
+
+
+def rotten_tomatoes(movie):
+    rel = movie.replace(" ","_").lower()
+    url = "https://www.rottentomatoes.com"
+    url = url + "/m/" + rel
+    hdr = {'User-Agent':'Mozilla/5.0'}
+    res = urllib2.urlopen(req).read().decode('utf-8')
+    regreg = re.findall('audiencescore="(.*?)"',res)
+    audiencescore = int(regreg[0])
+    regreg = re.findall('tomatometerscore="(.*?)"',res)
+    tomatometerscore = int(regreg[0])
+    return {"tomatometer score": tomatometerscore, "audience score": audiencescore}
 
 def rent_housing():
     import pandas as pd, datetime
