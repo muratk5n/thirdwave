d5e2c949b5a8f919bd7a5110e9cdbcdd82caace9
MK <mm@post.com>
2023-11-11 07:13:54 +0300

    --

--- en/mbl/2023/ukrmap.js
+++ en/mbl/2023/ukrmap.js
@@ -1,7 +1,7 @@
 
 function plot(before,after) {
     
-    url = `https://raw.githubusercontent.com/muratk5n/thirdwave/master/en/mbl/2023/ukrdata/${before}`;
+    url = `en/mbl/${before}`;
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", url = url, false ); 
     xmlHttp.send( null );
@@ -11,17 +11,32 @@ function plot(before,after) {
     res.forEach(function(x) {
 	bef.push([x[1],x[0]]);
     });    
-    var line = new L.Polyline(bef, {
+    var linebef = new L.Polyline(bef, {
 	color: 'darkblue', weight: 2, opacity: 0.5, smoothFactor: 1
     });
-    line.addTo(map);
+    linebef.addTo(map);
+    
+    url = `en/mbl/${after}`;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url = url, false ); 
+    xmlHttp.send( null );
+    result = xmlHttp.responseText;
+    res = JSON.parse(result);
+    aft = []
+    res.forEach(function(x) {
+	aft.push([x[1],x[0]]);
+    });    
+    var lineaft = new L.Polyline(aft, {
+	color: 'red', weight: 2, opacity: 0.5, smoothFactor: 1
+    });
+    lineaft.addTo(map);
     
 }
 
 function init() {
     map = L.map('map').setView([51,44], 5);
     
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: 'OSM'
     }).addTo(map);
